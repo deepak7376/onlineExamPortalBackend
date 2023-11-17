974b43c9ae1cb25cec83025346ab9e68f3ce2ba2
Deepak Yadav <deepak.y@activa.one>
2023-11-17 21:20:24 +0530

    updated the code

--- app_backend/exam/models.py
+++ app_backend/exam/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-    ROLE_CHOICES = [
-        ('admin', 'Admin'),
-        ('student', 'Student'),
-    ]
-    SUBSCRIPTION_STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('expired', 'Expired'),
-        ('canceled', 'Canceled'),
-    ]
-
-    status_name = models.CharField(max_length=255, choices=SUBSCRIPTION_STATUS_CHOICES)
-    role_name = models.CharField(max_length=255, choices=ROLE_CHOICES)
     username = models.CharField(max_length=255)
     full_name = models.CharField(max_length=255)
     contact_details = models.CharField(max_length=255)
@@ -26,36 +14,16 @@ class User(models.Model):
 
 
 class Question(models.Model):
-    QUESTION_TYPES = [
-        ('multiple_choice', 'Multiple Choice'),
-        ('true_false', 'True/False'),
-        # Add other question types as needed
-    ]
-    DIFFICULTY_LEVEL = [
-        ('easy', 'Easy'),
-        ('medium', 'Medium'),
-        ('hard', 'Hard'),
-    ]
-
     question_text = models.TextField()
-    question_tag = models.CharField(max_length=255, blank=True)
-    question_type = models.CharField(max_length=255, choices=QUESTION_TYPES)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    difficulty_level = models.CharField(max_length=255, choices=DIFFICULTY_LEVEL)
-    option_text = models.JSONField(default=list)
-    correct_answer = models.JSONField(default=list)
-
+    option_text = models.TextField(default=list)
+    correct_answer = models.TextField(default=list)
     
+
 class Exam(models.Model):
     EXAM_TYPE_CHOICES = [
         ('subjectwise', 'Subject Wise'),
         ('mock', 'Mock'),
         ('premium', 'Premium'),
-    ]
-    CATEGORY_TYPES = [
-        ('Science', 'Science'),
-        ('Mathematics', 'Mathematics'),
-        ('OnlineExam', 'OnlineExam'),
     ]
     CATEGORY_NAMES = [
         ('SSC', 'SSC'),
@@ -63,14 +31,12 @@ class Exam(models.Model):
         ('CAT', 'CAT'),
     ]
 
-    category_name = models.CharField(max_length=255, choices=CATEGORY_NAMES)
-    category_type = models.CharField(max_length=255, choices=CATEGORY_TYPES)
     title = models.CharField(max_length=255)
     description = models.TextField()
     exam_type = models.CharField(max_length=255, choices=EXAM_TYPE_CHOICES)
+    category_name = models.CharField(max_length=255, choices=CATEGORY_NAMES)
     duration = models.PositiveIntegerField()
     pass_mark = models.PositiveIntegerField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateTimeField()
     code = models.CharField(max_length=255)
 
